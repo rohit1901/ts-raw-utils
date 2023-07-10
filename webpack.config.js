301520614dbfc31a712279bfc471b61efa55655c
Rohit Khanduri <rohit.khanduri@hotmail.com>
2023-07-10 17:39:44 +0200

    [NEW] Added config files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-library.min.js',
+        library: 'MyLibrary',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: true,
+    },
+};
