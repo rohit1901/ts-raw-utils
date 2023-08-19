9f67f363388e6779228cb7bbe98ba159ff654b51
rohit1901 <rohit1901@users.noreply.github.com>
2023-08-19 12:39:56 +0000

    Prettified Code!

--- src/__tests__/promisify.spec.ts
+++ src/__tests__/promisify.spec.ts
@@ -1,27 +1,22 @@
-import {Promisify} from "../";
-describe('Promisify', () => {
-    const mockAsyncFunction = jest.fn().mockImplementation((arg: number, callback: (result: string) => void) => {
-        callback(`Processed: ${arg}`);
+import { Promisify } from "../";
+describe("Promisify", () => {
+  const mockAsyncFunction = jest
+    .fn()
+    .mockImplementation((arg: number, callback: (result: string) => void) => {
+      callback(`Processed: ${arg}`);
     });
 
-    it('should convert a callback-based function to a promise-based function', async () => {
-        const callbackMock = jest.fn();
-        const promisifiedFn = Promisify(mockAsyncFunction);
-        const promise = promisifiedFn(123).then(callbackMock);
+  it("should convert a callback-based function to a promise-based function", async () => {
+    const callbackMock = jest.fn();
+    const promisifiedFn = Promisify(mockAsyncFunction);
+    const promise = promisifiedFn(123).then(callbackMock);
 
-        await Promise.resolve(); // Resolve any pending microtasks
+    await Promise.resolve(); // Resolve any pending microtasks
 
-        expect(mockAsyncFunction).toHaveBeenCalledWith(123, expect.any(Function));
+    expect(mockAsyncFunction).toHaveBeenCalledWith(123, expect.any(Function));
 
-        await promise;
+    await promise;
 
-        expect(callbackMock).toHaveBeenCalledWith('Processed: 123');
-    });
+    expect(callbackMock).toHaveBeenCalledWith("Processed: 123");
+  });
 });
-
-
-
-
-
-
-
--- src/__tests__/writable.spec.ts
+++ src/__tests__/writable.spec.ts
@@ -1,21 +1,21 @@
-import { Writable} from '../';
+import { Writable } from "../";
 // Utility function: writable
-describe('Writable', () => {
-    interface ExampleInterface {
-        readonly prop1: string;
-        prop2: number;
-    }
+describe("Writable", () => {
+  interface ExampleInterface {
+    readonly prop1: string;
+    prop2: number;
+  }
 
-    it('should remove readonly modifier from properties', () => {
-        const writableObj: Writable<ExampleInterface> = {
-            prop1: 'value',
-            prop2: 123,
-        };
+  it("should remove readonly modifier from properties", () => {
+    const writableObj: Writable<ExampleInterface> = {
+      prop1: "value",
+      prop2: 123,
+    };
 
-        writableObj.prop1 = 'new value';
-        writableObj.prop2 = 456;
+    writableObj.prop1 = "new value";
+    writableObj.prop2 = 456;
 
-        expect(writableObj.prop1).toBe('new value');
-        expect(writableObj.prop2).toBe(456);
-    });
+    expect(writableObj.prop1).toBe("new value");
+    expect(writableObj.prop2).toBe(456);
+  });
 });
